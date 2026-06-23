0b6dad20a6398266597a1f41acbb77b6f57a4bc1
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-23 13:50:41 +0000

    cleanup - separate files

---
+++ arrays/day_of_week.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    int day;
+    cin >> day;
+switch(day){
+   case 1 : 
+   cout <<"monday"<<"\n";
+   break;
+   case 2 :
+   cout <<"tuesday"<<"\n";
+   break;
+   case 3 :
+   cout <<"wednesday"<<"\n";
+   break;
+   case 4 :
+   cout <<"thursday"<<"\n";
+   break;
+   case 5 :
+   cout <<"friday"<<"\n";
+   break;
+   case 6 :
+   cout <<"saturday"<<"\n";
+   break;
+   case 7 :
+   cout <<"sunday"<<"\n";
+   break;
+   default :
+   cout <<"invalid"<<"\n";
+}
+    return 0;
+}
