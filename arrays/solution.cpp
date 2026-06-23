6405c1aca057d2f6cd1ad53c0671e2c39c690c55
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-23 11:46:55 +0000

    day 1 - age job eligibility problem

--- arrays/solution.cpp
+++ arrays/solution.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int main() {
-    int a;
-    cout <<"enter your age"<<"\n";
-    cin >>a;
-    if(a<18)
-    {
-        cout << "you are not adult"<<"\n";
+    int age;
+    cout <<"enter your age" <<"\n";
+    cin >> age;
+    if(age<18){
+        cout <<"not eligible for job"<<"\n";
     }
-    else{
-        cout <<"you are adult"<<"\n";
+    else if(age<55){
+        cout <<"eligible for job"<<"\n";
     }
+    else if(age<=57){
+        cout <<"eligible for job , but retirement soon"<<"\n";
+    }
+    else {
+    cout <<"retirement time"<<"\n";
+    }
+
     return 0;
 }
+
