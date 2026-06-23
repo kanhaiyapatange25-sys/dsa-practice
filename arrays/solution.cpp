0165218b87429c56e5f6c4b9cb1d225f4c71389b
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-23 09:27:42 +0000

    day 1 - adult age check problem

--- arrays/solution.cpp
+++ arrays/solution.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << endl;
+    int a;
+    cout <<"enter your age"<<"\n";
+    cin >>a;
+    if(a<18)
+    {
+        cout << "you are not adult"<<"\n";
+    }
+    else{
+        cout <<"you are adult"<<"\n";
+    }
     return 0;
 }
