84e838cdfccd68b6f50ace7eb3a90b625e834911
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-24 16:26:55 +0000

    day 2 - pattern 1

---
+++ arrays/pattern1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+for(int j=0;j<n;j++){
+  cout << "*"<<" ";
+    }
+     cout <<endl;
+}
+}
+int main()
+{int n;
+    cout <<"enter the number"<<endl;
+    cin >> n;
+    pattern(n);
+    return 0;
+
+}
