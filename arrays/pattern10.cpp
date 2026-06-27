227f7cae1008f749fa262fb035d888366e11c17e
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-27 17:18:26 +0000

    pattern10 - triangle pattern

--- arrays/pattern10.cpp
+++ arrays/pattern10.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void pattern2(int n)
+void pattern10(int n)
 {
     for(int i=0;i<n;i++)
     {
@@ -10,12 +10,22 @@ void pattern2(int n)
         }
         cout <<endl;
     }
-}
+    for(int i=0;i<n-1;i++){
+
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout <<"*";
+        }
+        cout <<endl;
+    }
+
+    }
+
     int main()
     {
         int n;
   cout <<"enter the number"<<endl;
   cin >>n;
-  pattern2(n);
+  pattern10(n);
   return 0;
 }
