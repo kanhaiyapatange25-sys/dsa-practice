d738d6120f31a2b0518fe5a4848f2710dd8f6516
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-06-27 16:59:01 +0000

    pattern8 - diamond pattern

---
+++ arrays/pattern9.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern8(int n)
+{
+
+for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {cout<<" ";}
+    for(int k=0;k<2*i+1;k++)
+    {cout << "*";}
+    cout <<endl;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout <<" ";
+        }
+        for (int k=0;k<2*n-2*i-1;k++){
+            cout << "*";
+        }
+
+        cout <<endl;
+    }
+
+}
+int main()
+{
+    int n;
+    cout << "enter the number"<<endl;
+    cin >> n;
+    pattern8(n);
+    return 0;
+}
