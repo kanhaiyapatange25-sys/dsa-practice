46485fe2ff7e9015d43d167a58b5c624a46b4a19
kanhaiyapatange25-sys <kanhaiyapatange25@gmail.com>
2026-07-01 01:30:53 +0000

    pattern14 - alphabet triangle

---
+++ arrays/pattern14.cpp
@@ -0,0 +1,23 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+
+void pattern14(int n)
+{ 
+    for(int  i = 0 ; i < n ; i++)
+    { char c = 'A';
+        for (int j = 0 ; j <= i ; j++)
+        {cout << c++;
+    }
+
+    cout <<"\n";
+    }
+
+}
+int main()
+{
+    int n;
+    cout << "enter the number"<<"\n";
+    cin >> n;
+    pattern14(n);
+    return 0;
+}
